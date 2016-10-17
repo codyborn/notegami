47a5f575ee9093a56e3c779a9e614e975cccc553
Cody Born <codyborn@gmail.com>
2016-10-16 17:54:46 -0700

    initial project checkin

---
+++ AbyssTest/CopyIndexToNewTable.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using WebRole;
+using WebRole.Models;
+
+namespace AbyssTest
+{
+    [TestClass]
+    public class CopyIndexToNewTable
+    {
+        [TestMethod]
+        public void CopyHistory()
+        {
+            // foreach user
+            IEnumerable<User> allUsers = TableStore.GetAllEntitiesInAPartition<User>(TableStore.TableName.users, Constant.UserPartition);
+            foreach (User user in allUsers)
+            {
+                try
+                {
+                    IEnumerable<RecentTokenList> allNotes = TableStore.GetAllEntitiesInAPartition<RecentTokenList>(TableStore.TableName.notes, user.UserId);                    
+                    List<TableEntity> recentTokenList = new List<TableEntity>();
+                    // copy all user indices to new table
+                    foreach (TableEntity entity in allNotes)
+                    {
+                        if (!entity.RowKey.StartsWith("note_"))
+                        {
+                            if (entity.RowKey.Contains("rec"))
+                            {
+                                recentTokenList.Add((RecentTokenList)entity);
+                            }
+                        }
+                    }
+                    TableStore.BatchInsertOrUpdate(TableStore.TableName.recentTokens, recentTokenList);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+        [TestMethod]
+        public void CopyIndexNotes()
+        {
+            // foreach user
+            IEnumerable<User> allUsers = TableStore.GetAllEntitiesInAPartition<User>(TableStore.TableName.users, Constant.UserPartition);
+            foreach (User user in allUsers)
+            {
+                try
+                {
+                    IEnumerable<TableEntity> allNotes = TableStore.GetAllEntitiesInAPartition<TableEntity>(TableStore.TableName.notes, user.UserId);
+                    List<TableEntity> insertUpdateList = new List<TableEntity>();
+                    List<TableEntity> recentTokenList = new List<TableEntity>();
+                    // copy all user indices to new table
+                    foreach (TableEntity entity in allNotes)
+                    {
+                        if (!entity.RowKey.StartsWith("note_"))
+                        {
+                            if (entity.RowKey.Contains("rec"))
+                            {
+                                recentTokenList.Add((RecentTokenList)entity);
+                            }
+                            Index index = entity as Index;
+                            if (index == null)
+                            {
+                                Console.WriteLine("Cannot convert to index");
+                            }
+                            else
+                            {
+                                insertUpdateList.Add(index);
+                            }
+                        }
+                    }
+                    TableStore.BatchInsertOrUpdate(TableStore.TableName.indices, insertUpdateList);
+                    TableStore.BatchInsertOrUpdate(TableStore.TableName.recentTokens, recentTokenList);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+    }
+}
---
+++ AbyssTest/WebApiTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebRole;
+using WebRole.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace AbyssTest
+{
+    [TestClass]
+    public class WebApiTests
+    {
+        static string testUserId = "5459e17c33764954a9e4f8469d7e643d";
+        [TestMethod]
+        public void QueryNotesTest()
+        {
+            List<long> times = new List<long>();
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 10; i++)
+            {
+                sw.Start();
+                IEnumerable<Note> results = IndexerBase.QueryNotes(testUserId, "@bellevue");
+                Assert.IsTrue(results.Count() > 0);
+                sw.Stop();
+                times.Add(sw.ElapsedMilliseconds);
+                sw.Reset();
+            }
+            long avgTime = (long)times.Average<long>((n) => n);
+        }
+    }
+}
